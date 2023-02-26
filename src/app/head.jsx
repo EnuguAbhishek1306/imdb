a0f1a4f0fdde602f8729a659a818657d9f96f0ee
Abhishek Reddy <abhishekreddy1306@gmail.com>
2023-02-26 12:12:27 +0530

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+      <title> IMDB clone</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="This is the imdb clone" />
+    </>
+  )
+}
