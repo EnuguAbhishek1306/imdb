09d6cdb31195b4decd792e734ace8113c98c891b
Abhishek reddy <abhishekreddy1306@gmail.com>
2023-03-03 18:45:51 +0530

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-5">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+        pariatur dolorem laborum magni eaque voluptatibus laudantium quis.
+        Rerum, explicabo! Minima esse velit, nihil totam nam fugiat porro dolor
+        amet voluptate. Lorem ipsum dolor, sit amet consectetur adipisicing
+        elit. Fugit ipsa vel, vitae soluta impedit nam libero error ratione
+        consequatur repudiandae excepturi aperiam saepe blanditiis
+      </p>
+      <p>
+        numquam dicta blanditiis? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Placeat, minima. Labore qui nostrum ratione quo
+        inventore exercitationem minima iure iste, dicta totam nobis deleniti
+        adipisci magnam eum minus perspiciatis autem. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Eius molestias assumenda culpa
+        eligendi alias quidem quaerat quod,
+      </p>
+      <p>
+        voluptatibus maiores distinctio! Tenetur reiciendis labore unde enim,
+        esse ab explicabo quisquam at. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Corporis minima mollitia cupiditate animi facilis
+        laudantium? Aliquam nesciunt aspernatur porro nobis, sint quae, in eaque
+        iusto reprehenderit,
+      </p>
+      <p>
+        illo asperiores labore sit? Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Libero dolorem deserunt amet quos perspiciatis
+        voluptates! Exercitationem nesciunt aperiam culpa. Quos, ducimus? Dicta
+        quisquam ullam repellat id excepturi dolorum at quam.
+      </p>
+    </div>
+  );
+}
